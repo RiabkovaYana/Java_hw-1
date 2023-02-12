60ae759a7000cc88f7b6cc020f6d0138c4664cea
RiabkovaYana <123588181+RiabkovaYana@users.noreply.github.com>
2023-02-11 23:59:22 -1200

    Add files via upload

---
+++ myhomework.java
@@ -0,0 +1,113 @@
+package com;
+
+public class myhomework {
+    public static void main(String[] args) {
+
+      int a = 333;
+      System.out.println("Result (16 -> 10) = " + (3*Math.pow(16,2) + 3*Math.pow(16,1) + 3*Math.pow(16,0)));
+//    Result (16 -> 10) = 819.0
+
+
+
+        int a = 637;
+ // 10 -> 16
+        System.out.println(637 / 16 );    //39
+        System.out.println(637 % 16);        //13
+        System.out.println();
+
+        System.out.println(39 / 16);      //2
+        System.out.println(39 % 16);         //7
+        System.out.println();
+
+        System.out.println(2 / 16);       //0
+        System.out.println(2 % 16);          //2
+        System.out.println();
+
+        int a16 = 0x27d;
+        System.out.println("Result 637 (10 -> 16) = " + a16);
+ // 16 -> 10
+        System.out.println("Result (16 -> 10) = " + (2*Math.pow(16,2) + 7*Math.pow(16,1) + 13*Math.pow(16,0)));
+ //  10 -> 2
+        System.out.println(637 / 2);     //318
+        System.out.println(637 % 2);       //1
+        System.out.println();
+
+        System.out.println(318 / 2);     //159
+        System.out.println(318 % 2);      //0
+        System.out.println();
+
+        System.out.println(159 / 2);     //79
+        System.out.println(159 % 2);      //1
+        System.out.println();
+
+        System.out.println(79 / 2);      //39
+        System.out.println(79 % 2);       //1
+        System.out.println();
+
+        System.out.println(39 / 2);      //19
+        System.out.println(39 % 2);       //1
+        System.out.println();
+
+        System.out.println(19 / 2);      //9
+        System.out.println(19 % 2);       //1
+        System.out.println();
+
+        System.out.println(9 / 2);       //4
+        System.out.println(9 % 2);        //1
+        System.out.println();
+
+        System.out.println(4 / 2);       //2
+        System.out.println(4 % 2);        //0
+        System.out.println();
+
+        System.out.println(2 / 2);      //1
+        System.out.println(2 % 2);       //0
+        System.out.println();
+
+        System.out.println(1 / 2);      //0
+        System.out.println(1 % 2);       //1
+        System.out.println();
+
+        int a2 = 0b1001111101;
+        System.out.println("Result 637 (10 -> 2) = " + a2);
+
+ //     10 -> 3
+        System.out.println(637 / 3);    // 212
+        System.out.println(637 % 3);      //1
+        System.out.println();
+
+        System.out.println(212 / 3);     //70
+        System.out.println(212 % 3);      //2
+        System.out.println();
+
+        System.out.println(70 / 3);     //23
+        System.out.println(70 % 3);       //1
+        System.out.println();
+
+        System.out.println(23 / 3);     //7
+        System.out.println(23 % 3);       //2
+        System.out.println();
+
+        System.out.println(7 / 3);     //2
+        System.out.println(7 % 3);      //1
+        System.out.println();
+
+        System.out.println(2 / 3);     //0
+        System.out.println(2 % 3);       //2
+        System.out.println();
+
+        int a3 = 212121;
+        System.out.println("Result 637 (10 -> 3) = " + a3);
+
+
+        int a2 = 0b1111011;
+        System.out.println("Result 123 (10 -> 2) = " + a2);
+ //     2 -> 10
+        int a = 11100111;
+        System.out.println("Result (2 -> 10) = " +
+                (1*Math.pow(2,7) + 1*Math.pow(2,6) + 1*Math.pow(2,5) + 0*Math.pow(2,4) +
+                                0*Math.pow(2,3) + 1*Math.pow(2,2) + 1*Math.pow(2,1) + 1*Math.pow(2,0)));
+ //     Result (2 -> 10) = 231.0
+
+    }
+}
